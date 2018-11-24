84c4a135c65b2f0967817f46096e2b785399f6fc
Piotr Wolny <pwolny@e-point.pl>
2018-11-24 13:29:50 +0100

    Walidacja formularza rejestracji.

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { User } from './user';
 
 @Component({
   selector: 'app-register',
@@ -10,11 +11,11 @@ import { HttpClient } from '@angular/common/http';
 export class RegisterComponent implements OnInit {
 
   registerForm = this.formBuilder.group({
-    login: [''],
-    password: [''],
-    email: [''],
-    firstName: [''],
-    lastName: [''],
+    login: ['', [Validators.required, Validators.maxLength(50)]],
+    password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(100)]],
+    email: ['', [Validators.required, Validators.email, Validators.maxLength(254)]],
+    firstName: ['', [Validators.required, Validators.maxLength(50)]],
+    lastName: ['', [Validators.required, Validators.maxLength(50)]],
   });
 
   constructor(private readonly formBuilder: FormBuilder,
@@ -24,7 +25,7 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    this.httpClient.post('/api/register', {}).subscribe(() => {
+    this.httpClient.post('/api/register', this.registerForm.value).subscribe(() => {
 
     });
   }
